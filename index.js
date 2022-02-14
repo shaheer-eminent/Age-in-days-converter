621d141cb9a39b4fb343e322d6a9dab136a5a1f7
Shaheer Inayat ali - Eminent <89388625+ShaheerInayatAli@users.noreply.github.com>
2022-02-14 10:46:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// Age in Days
+function ageInDays() {
+    let userAge = prompt("What's your current age?");
+    let ageDays = userAge * 365;
+    document.getElementById("age__result").innerText = "You are " + ageDays + " days old 🥳.";
+}
+
+function reset() {
+    document.getElementById("age__result").innerText ="Your result will show here";
+}
